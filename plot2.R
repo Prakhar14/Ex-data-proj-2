2b667361c982661402427adf9d44f72bac88a8ca
Prakhar14 <contactme.prakhar@gmail.com>
2014-08-25 02:04:24 +0530

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,27 @@
+
+# Course Project 2 - Plot 2
+# Assignment at
+# https://class.coursera.org/exdata-002/human_grading/view/courses/972082/assessments/4/submissions
+# Data file from
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+# See download_data.R
+
+# Question 2:
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland
+# (fips == "24510") from 1999 to 2008? Use the base plotting system to make a
+# plot answering this question.
+
+# Read the data file
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+BaltimoreCity <- subset(NEI, fips == "24510")
+
+totalPM25ByYear <- tapply(BaltimoreCity$Emissions, BaltimoreCity$year, sum)
+
+png("plot2.png")
+plot(names(totalPM25ByYear), totalPM25ByYear, type="l",
+     xlab="Year", ylab=expression("Total" ~ PM[2.5] ~ "Emissions (tons)"),
+     main=expression("Total Baltimore City" ~ PM[2.5] ~ "Emissions by Year"))
+dev.off()
